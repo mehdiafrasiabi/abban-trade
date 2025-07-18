e1d2a3558421a5266ffc3e65d9364b218c5299b2
Mehdi Afrasiabi <mr.abbann1976@gmail.com>
2025-07-19 03:11:55 +0430

    done project

--- app/Livewire/Client/Auth/ForgotPassword.php
+++ app/Livewire/Client/Auth/ForgotPassword.php
@@ -3,11 +3,25 @@
 namespace App\Livewire\Client\Auth;
 
 use Artesaos\SEOTools\Traits\SEOTools;
+
+use App\Models\User;
+use Carbon\Carbon;
 use Livewire\Component;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\RateLimiter;
 
 class ForgotPassword extends Component
 {
     use SEOTools;
+    public $step = 1;
+    public $mobile;
+    public $code;
+    public $generatedCode;
+    public $password;
+    public $password_confirmation;
+
+    public $successMessage;
     public function mount()
     {
         $this->seoConfig();
@@ -16,6 +30,76 @@ class ForgotPassword extends Component
     {
         $this->seo()->setTitle('فراموشی رمز عبور');
     }
+
+
+    public function sendCode()
+    {
+        $this->validate([
+            'mobile' => ['required', 'regex:/^09\d{9}$/', 'exists:users,mobile'],
+        ], [
+            'mobile.required' => 'شماره موبایل الزامی است.',
+            'mobile.regex' => 'فرمت شماره صحیح نیست.',
+            'mobile.exists' => 'کاربری با این شماره یافت نشد.',
+        ]);
+
+        // نرخ ارسال محدود شود (5 بار در 1 دقیقه مثلاً)
+        $key = 'send-otp:' . $this->mobile;
+        if (RateLimiter::tooManyAttempts($key, 5)) {
+            $this->dispatch('error', '⛔ ارسال بیش از حد مجاز. لطفا بعداً امتحان کنید.');
+            return;
+        }
+
+        RateLimiter::hit($key, 180); // 60 ثانیه
+
+        $this->generatedCode = rand(100000, 999999);
+
+        $user = User::where('mobile', $this->mobile)->first();
+        $user->notify(new \App\Notifications\SendOtpToUser($this->mobile, $this->generatedCode));
+        session()->put('reset_mobile', $this->mobile); // ذخیره برای مراحل بعد
+        $this->dispatch('success', ' درحال پردازش ...');
+        sleep(0.5);
+        $this->step = 2;
+    }
+
+    public function verifyCode()
+    {
+        if ($this->code == $this->generatedCode) {
+            $this->mobile = session()->get('reset_mobile');
+            $this->dispatch('success', ' درحال پردازش ...');
+            sleep(0.5);
+            $this->step = 3;
+
+        } else {
+            $this->dispatch('error', 'کد وارد شده نادرست است.');
+        }
+    }
+
+    public function resetPassword()
+    {
+        $this->validate([
+            'password' => 'required|min:6|confirmed',
+        ], [
+            'password.required' => 'رمز عبور الزامی است.',
+            'password.min' => 'رمز باید حداقل ۶ کاراکتر باشد.',
+            'password.confirmed' => 'رمزها یکسان نیستند.',
+        ]);
+        $mobile = session()->get('reset_mobile'); // گرفتن موبایل
+
+        $user = User::where('mobile', $mobile)->first();
+        if ($user) {
+            $user->password = Hash::make($this->password);
+            $user->save();
+
+            auth()->login($user); // ورود خودکار
+
+            $this->dispatch('success', '✅ رمز با موفقیت تغییر کرد!');
+
+            sleep(3);
+
+            return redirect()->to(route('client.profile.dashboard')); // مسیر دلخواه
+        }
+    }
+
     public function render()
     {
         return view('livewire.client.auth.forgot-password')->layout('layouts.client.auth');
--- resources/views/livewire/client/auth/forgot-password.blade.php
+++ resources/views/livewire/client/auth/forgot-password.blade.php
@@ -21,7 +21,7 @@
                     <div class="d-flex flex-column sign-in-container h-100">
                         <div class="align-items-center sign-in px-3 mx-auto my-auto">
                             <!-- Begin::form -->
-                            <form class="sign-in-form h-100">
+                            <div class="sign-in-form h-100">
                                 <div class="py-4">
                                     <a class="logo-link" href="{{route('client.home')}}">
                                         <img class="logo-image" alt="Logo" src="/client/assets/media/images/icons/logo.png" />
@@ -29,15 +29,48 @@
                                 </div>
 
                                 <h2 class="text-dark font-weight-bold pb-2">بازنشانی گذرواژه</h2>
+                                @if ($successMessage)
+                                    <div class="bg-success text-white p-4 rounded">{{ $successMessage }}</div>
+                                @endif
                                 <!-- Begin::form-group -->
+                                @if ($step === 1)
                                 <div class="form-group">
                                     <label class="d-flex" for="mobile">نام کابری</label>
-                                    <input dir="rtl" class="form-control form-control-lg box-rounded" type="tel" id="mobile" placeholder="شماره موبایل همراه خود را وارد کنید" />
+                                    <input maxlength="11" dir="rtl" class="form-control form-control-lg box-rounded" type="tel" id="mobile" name="mobile" wire:model="mobile" placeholder="شماره موبایل همراه خود را وارد کنید" />
+                                    @error('mobile') <span class="text-danger ">{{ $message }}</span> @enderror
+                                    <br>
+                                    <button  wire:click="sendCode" class="btn btn-main shadow-sm btn-block box-rounded" type="submit">مرحله بعد</button>
+                                    <br>
+                                    <a href="{{route('client.login')}}" class=" btn btn-warning">بازگشت</a>
                                 </div>
+                                @elseif($step == 2)
+                                    <div class="form-group">
+                                        <label class="d-flex" for="mobile">کد تایید</label>
+                                        <input dir="rtl" class="form-control form-control-lg box-rounded" type="tel" id="mobile" name="code" wire:model="code" placeholder="کد تایید 6 رقمی خود را وارد کنید" />
+                                        @error('code') <span class="text-danger text-sm">{{ $message }}</span> @enderror
+                                        <br>
+                                        <button  wire:click="verifyCode" class="btn btn-main shadow-sm btn-block box-rounded" type="submit">تایید</button>
+
+                                    </div>
+                                @elseif($step == 3)
+                                    <div>
+                                        <label>رمز عبور جدید:</label>
+                                        <input type="password" wire:model="password" class="form-control form-control-lg box-rounded">
+                                        @error('password') <span class="text-danger">{{ $message }}</span> @enderror
+                                        <br>
+                                        <label class="mt-3">تکرار رمز عبور:</label>
+                                        <input type="password" wire:model="password_confirmation" class="form-control form-control-lg box-rounded">
+                                        @error('password_confirmation') <span class="text-danger">{{ $message }}</span> @enderror
+
+                                        <br>
+                                        <button wire:click="resetPassword" class="btn btn-main shadow-sm btn-block box-rounded">
+                                            تغییر رمز عبور
+                                        </button>
+                                    </div>
+                                @endif
                                 <!-- End::form-group -->
 
-                                <button class="btn btn-main shadow-sm btn-block box-rounded" type="submit">مرحله بعد</button>
-                            </form>
+                            </div>
                             <!-- End::form -->
                         </div>
                     </div>
